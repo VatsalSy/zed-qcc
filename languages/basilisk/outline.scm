00365bf668a94abbe235a6b74ca6767df58814a3
Vatsal Sanjay <vatsalsanjay@gmail.com>
2025-10-25 20:30:58 +0100

    Extend outline patterns for functions and type declarations
    - Add function_definition patterns for pointer and parenthesized declarators
    - Capture function names in declarations like 'void* foo()' and 'void (*handler)()'
    - Add union_specifier and enum_specifier patterns to outline
    - Include unions and enums in symbol outline alongside existing struct declarations

--- languages/basilisk/outline.scm
+++ languages/basilisk/outline.scm
@@ -3,6 +3,26 @@
     declarator: (identifier) @name)
   (#set! kind "function"))
 
+(function_definition
+  declarator: (function_declarator
+    declarator: (pointer_declarator
+      declarator: (identifier) @name))
+  (#set! kind "function"))
+
+(function_definition
+  declarator: (function_declarator
+    declarator: (parenthesized_declarator
+      declarator: (identifier) @name))
+  (#set! kind "function"))
+
 (struct_specifier
   name: (type_identifier) @name
   (#set! kind "struct"))
+
+(union_specifier
+  name: (type_identifier) @name
+  (#set! kind "union"))
+
+(enum_specifier
+  name: (type_identifier) @name
+  (#set! kind "enum"))
