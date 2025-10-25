0bb4ef241d71b71895cbda277383459aaabc9909
Vatsal Sanjay <vatsalsanjay@gmail.com>
2025-10-25 20:30:04 +0100

    Replace custom highlight capture with standard @keyword in highlights.scm
    - Change @function.basilisk capture to @keyword for Basilisk constructs
    - Ensures event, foreach, foreach_face, foreach_dimension are styled by standard themes
    - Maintains @variable.other.member for field identifiers (x, y, z)
    - Fixes theme styling consistency for syntax highlighting

--- languages/basilisk/highlights.scm
+++ languages/basilisk/highlights.scm
@@ -1,6 +1,6 @@
 ; Highlight Basilisk-specific constructs on top of the C grammar
-((identifier) @function.basilisk
-  (#match? @function.basilisk "^(event|foreach|foreach_face|foreach_dimension)$"))
+((identifier) @keyword
+  (#match? @keyword "^(event|foreach|foreach_face|foreach_dimension)$"))
 
 ((field_identifier) @variable.other.member
   (#match? @variable.other.member "^(x|y|z)$"))
