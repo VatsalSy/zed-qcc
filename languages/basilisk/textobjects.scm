f16ccabc0427c6245ac9672e38fedecebf55c61e
Vatsal Sanjay <vatsalsanjay@gmail.com>
2025-10-25 20:30:39 +0100

    Add pointer and parenthesized declarator patterns to textobjects.scm
    - Add pattern for pointer_declarator functions: return_type (*name)(args)
    - Add pattern for parenthesized_declarator functions: return_type (name)(args)
    - Extends function definition matching to cover all three common declarator shapes
    - Ensures text object selection works for complex C function declarations

--- languages/basilisk/textobjects.scm
+++ languages/basilisk/textobjects.scm
@@ -2,3 +2,17 @@
   declarator: (function_declarator
     declarator: (identifier) @function.name)
   body: (compound_statement) @function.inside) @function.around
+
+; Pointer declarators: return_type (*function_name)(args)
+(function_definition
+  declarator: (function_declarator
+    declarator: (pointer_declarator
+      declarator: (identifier) @function.name))
+  body: (compound_statement) @function.inside) @function.around
+
+; Parenthesized declarators: return_type (function_name)(args)
+(function_definition
+  declarator: (function_declarator
+    declarator: (parenthesized_declarator
+      declarator: (identifier) @function.name))
+  body: (compound_statement) @function.inside) @function.around
