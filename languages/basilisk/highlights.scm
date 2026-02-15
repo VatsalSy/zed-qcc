5ca6582dfd47acb3982dc21eb794e45fccb4dea4
Vatsal Sanjay <vatsal.sanjay@comphy-lab.org>
2026-02-15 12:48:37 +0000

    Restore C syntax highlighting for Basilisk
    Replace the minimal highlight query with a full C baseline query and keep Basilisk-specific keyword highlighting on top.
    
    This restores normal syntax coloring for .c/.h buffers assigned to Basilisk C.

--- languages/basilisk/highlights.scm
+++ languages/basilisk/highlights.scm
@@ -1,6 +1,84 @@
-; Highlight Basilisk-specific constructs on top of the C grammar
-((identifier) @keyword
-  (#match? @keyword "^(event|foreach|foreach_face|foreach_dimension)$"))
+"break" @keyword
+"case" @keyword
+"const" @keyword
+"continue" @keyword
+"default" @keyword
+"do" @keyword
+"else" @keyword
+"enum" @keyword
+"extern" @keyword
+"for" @keyword
+"if" @keyword
+"inline" @keyword
+"return" @keyword
+"sizeof" @keyword
+"static" @keyword
+"struct" @keyword
+"switch" @keyword
+"typedef" @keyword
+"union" @keyword
+"volatile" @keyword
+"while" @keyword
 
-((field_identifier) @variable.other.member
-  (#match? @variable.other.member "^(x|y|z)$"))
+"#define" @keyword
+"#elif" @keyword
+"#else" @keyword
+"#endif" @keyword
+"#if" @keyword
+"#ifdef" @keyword
+"#ifndef" @keyword
+"#include" @keyword
+(preproc_directive) @keyword
+
+"--" @operator
+"-" @operator
+"-=" @operator
+"->" @operator
+"=" @operator
+"!=" @operator
+"*" @operator
+"&" @operator
+"&&" @operator
+"+" @operator
+"++" @operator
+"+=" @operator
+"<" @operator
+"==" @operator
+">" @operator
+"||" @operator
+
+"." @delimiter
+";" @delimiter
+
+(string_literal) @string
+(system_lib_string) @string
+
+(null) @constant
+(number_literal) @number
+(char_literal) @number
+
+(call_expression
+  function: (identifier) @function)
+(call_expression
+  function: (field_expression
+    field: (field_identifier) @function))
+(function_declarator
+  declarator: (identifier) @function)
+(preproc_function_def
+  name: (identifier) @function.special)
+
+(field_identifier) @property
+(statement_identifier) @label
+(type_identifier) @type
+(primitive_type) @type
+(sized_type_specifier) @type
+
+((identifier) @constant
+ (#match? @constant "^[A-Z][A-Z\\d_]*$"))
+
+((identifier) @keyword
+ (#match? @keyword "^(event|foreach|foreach_face|foreach_dimension|foreach_boundary|foreach_level|foreach_neighbor)$"))
+
+(identifier) @variable
+
+(comment) @comment
